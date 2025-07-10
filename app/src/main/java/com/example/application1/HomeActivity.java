c80f3e87244deb2685ea401b159cb27ae4c48747
minh <pqminh131204@gmail.com>
2025-07-10 17:52:25 +0700

    add dialog for handling Back button press

--- app/src/main/java/com/example/application1/HomeActivity.java
+++ app/src/main/java/com/example/application1/HomeActivity.java
@@ -1,11 +1,14 @@
 package com.example.application1;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -28,6 +31,31 @@ public class HomeActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.textView);
         String username = getIntent().getStringExtra("username");
         textView.setText("Welcome, " + username);
+
+        OnBackPressedCallback newCallback = new OnBackPressedCallback(true /* enabled by default */) {
+            @Override
+            public void handleOnBackPressed() {
+                new AlertDialog.Builder(HomeActivity.this)
+                        .setTitle("Exit app")
+                        .setMessage("Bạn có muốn rời khỏi ứng dụng này")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Disable this object/callback (newCallback)
+                                setEnabled(false);
+
+                                // Invoke Back Action => system find default OnBackPressedCallback instance => Invoke handleOnBackPressed => Back to system
+                                HomeActivity.super.getOnBackPressedDispatcher().onBackPressed();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .show();
+            }
+        };
+        getOnBackPressedDispatcher().addCallback(this, newCallback);
 
         logoutButton.setOnClickListener(v -> {
             Intent intent = new Intent(this, MainActivity.class);
